38883ae555b839bffe7004029f8202ee3e1d4930
Arkadiusz Plichta <el.pikel@gmail.com>
2019-11-18 15:32:35 +0100

    toad solution

--- Unit2.cs
+++ Unit2.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 
 namespace zsza
@@ -10,6 +11,33 @@ namespace zsza
         public void Permutations(int n, int [] numbers, string result)
         {
             Assert.Equal(result, IsPermutation(n, numbers));
+        }
+
+        [Theory]
+        [InlineData(5, 8, new [] { 1, 3, 1, 4, 2, 3, 5, 4 }, 7)]
+        [InlineData(5, 8, new [] { 1, 3, 1, 4, 2, 3, 4, 5 }, 8)]
+        [InlineData(3, 8, new [] { 1, 3, 1, 3, 2, 3, 1, 1 }, 5)]
+        public void Toad(int position, int leaves, int [] leavePositions, int result)
+        {
+            Assert.Equal(result, CalculateLeavesNumber(position, leaves, leavePositions));
+        }
+
+        private int CalculateLeavesNumber(int position, int leaves, int[] leavePositions)
+        {
+            var counts = new int[position + 1];
+            var positions = 0;
+
+            for (int i = 0; i < leaves; i++) {
+                counts[leavePositions[i]] += 1;
+
+                if (counts[leavePositions[i]] == 1)
+                    positions += 1;
+
+                if (positions == position)
+                    return i + 1;
+            }
+
+            return -1;
         }
 
         private string IsPermutation(int n, int [] numbers)
